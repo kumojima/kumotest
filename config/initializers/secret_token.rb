5e288a10dd214cff80af16116e4c630034788651
kumo <kumo@kumo-Prime-Series.(none)>
2012-04-03 01:26:03 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kumoapp::Application.config.secret_token = '2462a9e8c9c55daa0c698741db2107840665bdf66254dae82ed891177d1e9668e3638c79f0e1bb44940c160a83b01461e8fc29bc1ef0dce9694e1ba94f542436'
